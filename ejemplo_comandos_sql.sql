b514bd4c83a188512414bcc30016ab10394a9ddf
castro08 <miguelangelcastro200400@gmail.com>
2025-12-03 15:34:37 -0600

    se crea archivo sql con comandos DDL

---
+++ ejemplo_comandos_sql.sql
@@ -0,0 +1,12 @@
+-- DDL
+-- CREATE DATABASE crea una base de datos
+
+CREATE DATABASE testsDb;
+ CREATE DATABASE bbBorrar;
+ 
+ -- DROP DATABASE elimina una base de datos
+ DROP DATABASE bbBorrar;
+ 
+ 
+
+
